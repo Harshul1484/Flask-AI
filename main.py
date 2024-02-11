d0f28a18925f3d44c0fdc33b7f1b2e640a33cada
Harshul Rathod <118340577+Harshul1484@users.noreply.github.com>
2024-02-11 23:12:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,34 +64,23 @@
 
 # New Code
 from flask import Flask, jsonify, request
-from characterai import PyAsyncCAI
-import asyncio
+from characterai import PyCAI
 
 app = Flask(__name__)
 
-async def main():
-    client = PyAsyncCAI('34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a')
-
-    # Get chat information
+def get_chat_info():
+    token = "34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a"
+    client = PyCAI(token)
     char = "AbuIXFqY7EsLrOjUDLR0bDNYwqU6MDQyAKtzGIMeLI4"
-    chat = await client.chat.get_chat(char)
+    chat = client.chat.get_chat(char)
     participants = chat['participants']
-
+    
     if not participants[0]['is_human']:
         tgt = participants[0]['user']['username']
     else:
         tgt = participants[1]['user']['username']
-
-    # Chat loop
-    while True:
-        message = input('You: ')
-
-        data = await client.chat.send_message(chat['external_id'], tgt, message)
-
-        name = data['src_char']['participant']['name']
-        text = data['replies'][0]['text']
-
-        print(f"{name}: {text}")
+        
+    return chat, tgt, client
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -100,22 +89,12 @@ def home():
         return jsonify({'data': data})
 
 @app.route('/chat', methods=['POST'])
-async def chat():
+def chat():
     if request.method == 'POST' and 'msg' in request.json:
         msg = request.json['msg']
-        client = PyAsyncCAI('YOUR_TOKEN')
+        chat, tgt, client = get_chat_info()
 
-        char = "AbuIXFqY7EsLrOjUDLR0bDNYwqU6MDQyAKtzGIMeLI4"
-
-        chat = await client.chat.get_chat(char)
-        participants = chat['participants']
-
-        if not participants[0]['is_human']:
-            tgt = participants[0]['user']['username']
-        else:
-            tgt = participants[1]['user']['username']
-
-        data = await client.chat.send_message(chat['external_id'], tgt, msg)
+        data = client.chat.send_message(chat['external_id'], tgt, msg)
 
         name = data['src_char']['participant']['name']
         text = data['replies'][0]['text']
@@ -125,5 +104,4 @@ async def chat():
         return jsonify({'error': 'Invalid request or missing "msg" in JSON payload'})
 
 if __name__ == '__main__':
-    asyncio.run(main())
     app.run(debug=False, host='0.0.0.0')
