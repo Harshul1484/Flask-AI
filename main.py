4d419f0231fe0366a3c4f89242defa0bbf7dd8e9
Harshul Rathod <118340577+Harshul1484@users.noreply.github.com>
2024-02-12 10:14:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def home():
 
 @app.route('/chat/<string:msg>', methods=['GET'])
 def disp(msg):
-    token = 34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a
+    token = "34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a"
     client = PyCAI(token)
 
     char = AbuIXFqY7EsLrOjUDLR0bDNYwqU6MDQyAKtzGIMeLI4
@@ -35,7 +35,7 @@ def disp(msg):
 def chat():
     if request.method == 'POST' and 'msg' in request.json:
         msg = request.json['msg']
-        token = 34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a
+        token = "34f5788ba1f0cfcb8b8f03437c732bad6e0abd2a"
         client = PyCAI(token)
 
         char = AbuIXFqY7EsLrOjUDLR0bDNYwqU6MDQyAKtzGIMeLI4
