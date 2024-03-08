64575004d7e8029380e1ae50e37f9f105039b48c
Harshul Rathod <118340577+Harshul1484@users.noreply.github.com>
2024-03-08 18:32:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Retrieve the API token and char from environment variables
 token = os.environ.get('CHARACTERAI_API_TOKEN')
-char = os.environ.get('CHARACTERAI_CHAR_ID')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -18,7 +17,31 @@ def home():
 def disp(msg):
     client = PyCAI(token)
 
-    try:
+    char = os.environ.get('CHARACTERAI_CHAR_ID')
+
+    chat = client.chat.get_chat(char)
+    participants = chat['participants']
+
+    if not participants[0]['is_human']:
+        tgt = participants[0]['user']['username']
+    else:
+        tgt = participants[1]['user']['username']
+
+    data = client.chat.send_message(chat['external_id'], tgt, msg)
+
+    name = data['src_char']['participant']['name']
+    text = data['replies'][0]['text']
+
+    return jsonify({'data': text})
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    if request.method == 'POST' and 'msg' in request.json:
+        msg = request.json['msg']
+        client = PyCAI(token)
+
+        char = os.environ.get('CHARACTERAI_CHAR_ID')
+
         chat = client.chat.get_chat(char)
         participants = chat['participants']
 
@@ -32,38 +55,9 @@ def disp(msg):
         name = data['src_char']['participant']['name']
         text = data['replies'][0]['text']
 
-        return jsonify({'data': text})
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    if request.method == 'POST' and 'msg' in request.json:
-        msg = request.json['msg']
-        client = PyCAI(token)
-
-        try:
-            chat = client.chat.get_chat(char)
-            participants = chat['participants']
-
-            if not participants[0]['is_human']:
-                tgt = participants[0]['user']['username']
-            else:
-                tgt = participants[1]['user']['username']
-
-            data = client.chat.send_message(chat['external_id'], tgt, msg)
-
-            name = data['src_char']['participant']['name']
-            text = data['replies'][0]['text']
-
-            return jsonify({'reply': text})
-
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
-
+        return jsonify({'reply': text})
     else:
-        return jsonify({'error': 'Invalid request or missing "msg" in JSON payload'}), 400
+        return jsonify({'error': 'Invalid request or missing "msg" in JSON payload'})
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
